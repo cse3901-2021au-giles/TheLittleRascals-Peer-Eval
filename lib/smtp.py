33f21eb2f9ef4c410519dc38707adba959c827de
Stuart Burgdoerfer <57853652+sinjaxbravo@users.noreply.github.com>
2021-11-30 12:27:30 -0500

    Update smtp.py

--- lib/smtp.py
+++ lib/smtp.py
@@ -10,30 +10,30 @@ try:
     server.ehlo()
     server.login(gmail_user, gmail_password)
 
-    to_addr = sys.argv[1]
-    from_addr = "Little Rascals <littlerascalsfall21@gmail.com>"
+    to = sys.argv[1]
+    fromEmail = "Little Rascals <littlerascalsfall21@gmail.com>"
     msg_type = sys.argv[2]
 
     if int(msg_type) == 1:
-        subject_of_message = "Form successfully submitted"
-        date_and_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
-        message_text = "Congrats!\nYou submitted a form at %s\n\n-Littlerascals :)" % date_and_time
-        message = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % (from_addr, to_addr, subject_of_message,
-                                                                     date_and_time, message_text)
+        subject = "Form successfully submitted"
+        date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
+        message_text = "Congrats!\nYou submitted a form at %s\n\n-Littlerascals :)" % date
+        message = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % (fromEmail, to, subject,
+                                                                     date, message_text)
     elif int(msg_type) == 2:
-        subject_of_message = "You have successfully created an account"
-        date_and_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
-        message_text = "Account created at %s for email: %s \n\n-Littlerascals :)" % (date_and_time, to_addr)
-        message = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % (from_addr, to_addr, subject_of_message,
-                                                                     date_and_time, message_text)
+        subject = "You have successfully created an account"
+        date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
+        message_text = "Account created at %s for email: %s \n\n-Littlerascals :)" % (date, to)
+        message = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % (fromEmail, to, subject,
+                                                                     date, message_text)
     elif int(msg_type) == 3:
-        subject_of_message = "You have been assigned a new poll"
-        date_and_time = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
-        message_text = "A new poll assignment was created at %s\n\n-Littlerascals" % date_and_time
-        message = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % (from_addr, to_addr, subject_of_message,
-                                                                     date_and_time, message_text)
+        subject = "You have been assigned a new poll"
+        date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M")
+        message_text = "A new poll assignment was created at %s\n\n-Littlerascals" % date
+        message = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s" % (fromEmail, to, subject,
+                                                                     date, message_text)
 
-    server.sendmail(from_addr, to_addr, message)
+    server.sendmail(fromEmail, to, message)
     server.quit()
 except:
     print('Error sending email! Make sure that python is installed and on path!')
